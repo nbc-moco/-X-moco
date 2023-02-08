0c01ef95b50c2281dfce2fa61409a1016fab0725
Minwoozzang <kminu0819@naver.com>
2023-02-08 16:52:12 +0900

    edit. 수정

--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 export const HeaderBody = styled.div`
   width: 100%;
   height: 65px;
-  background-color: #0006;
+  background-color: #0005;
 
   display: flex;
   justify-content: center;
