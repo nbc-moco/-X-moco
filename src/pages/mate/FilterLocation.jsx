f07aa1a96a5f72994868eb85a395e0d56cac51f7
wallofwolf <77008335+wallofwolf@users.noreply.github.com>
2023-02-13 00:16:11 +0900

    desgin : MateList에 필터/정렬 UI 추가

---
+++ src/pages/mate/FilterLocation.jsx
@@ -0,0 +1,27 @@
+import Select from 'react-select';
+import { useState } from 'react';
+
+const FilterLocation = () => {
+  const options = [
+    { value: '마포구', label: '마포구' },
+    { value: '용산구', label: '용산구' },
+    { value: '종로구', label: '종로구' },
+  ];
+  const [text, setText] = useState('');
+  console.log(text);
+  const handleSelect = (e) => {
+    setText(e);
+  };
+
+  return (
+    <Select
+      options={options}
+      placeholder="지역"
+      onChange={handleSelect}
+      value={text}
+      // unstyled={true}
+    />
+  );
+};
+
+export default FilterLocation;
--- src/pages/mate/MateList.jsx
+++ src/pages/mate/MateList.jsx
@@ -1,10 +1,29 @@
 import styled from '@emotion/styled';
 import CardSection from '../../shared/CardSection';
+import FilterLocation from './FilterLocation';
+import FilterNumOfMember from './FilterNumOfMember';
+import FilterTech from './FilterTech';
+import FilterTime from './FilterTime';
 
 const MateList = () => {
   const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+
   return (
     <>
+      {/* 필터 & 정렬 */}
+      <ViewOptions>
+        <FilterBox>
+          <FilterTech />
+          <FilterLocation />
+          <FilterTime />
+          <FilterNumOfMember />
+        </FilterBox>
+        <SortBox>
+          <SortByRecommend>추천순</SortByRecommend>
+          <SortByNew>최신순</SortByNew>
+        </SortBox>
+      </ViewOptions>
+
       {/* 카드 리스트 */}
       <CardListContainer>
         <CardList>
@@ -19,13 +38,41 @@ const MateList = () => {
 
 export default MateList;
 
+const ViewOptions = styled.div`
+  max-width: 1200px;
+  padding: 1em;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const FilterBox = styled.div`
+  display: flex;
+  gap: 1em;
+`;
+
+const SortBox = styled.div`
+  display: flex;
+  gap: 1em;
+`;
+
+const SortByRecommend = styled.div`
+  cursor: pointer;
+`;
+const SortByNew = styled.div`
+  cursor: pointer;
+`;
+
 const CardListContainer = styled.section`
   max-width: 1200px;
-  margin: auto;
+  margin: 0 auto;
 `;
 
 const CardList = styled.div`
+  margin: 0 auto;
   display: flex;
+  justify-content: center;
   flex-direction: row;
   flex-wrap: wrap;
   gap: 1em;
