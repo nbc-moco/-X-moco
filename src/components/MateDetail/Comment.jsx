bef4e20e79ec32ccb6dfd9d46498127194026418
2sel <yunwel0610@gmail.com>
2023-02-13 11:05:04 +0900

    fix. comment form으로 변경

--- src/components/MateDetail/Comment.jsx
+++ src/components/MateDetail/Comment.jsx
@@ -6,11 +6,15 @@ const Comment = () => {
     <CommentContainer>
       <CommentContainHeader>댓글</CommentContainHeader>
       {/* 댓글 내용 */}
-      <CommentWrap>
-        <CommentUserTitle>사용자</CommentUserTitle>
-        <CommentUserWriting>스터디룸으로 모임하기 좋아요</CommentUserWriting>
-        <CommentDate>23.01.31 12:34</CommentDate>
-        <CommentButton>답글쓰기</CommentButton>
+      <CommentWrap
+      // onSubmit={}
+      >
+        <CommentUserName>사용자 이름{/* {comment.userName} */}</CommentUserName>
+        <CommentUserWriting
+          placeholder="댓글을 남겨보세요"
+          //  onChange={}
+        />
+        <CommentButton>등록하기</CommentButton>
       </CommentWrap>
     </CommentContainer>
   );
@@ -28,24 +32,21 @@ const CommentContainHeader = styled.p`
   font-size: 24px;
   font-weight: 500;
   line-height: 29px;
+  margin-bottom: 30px;
 `;
-const CommentWrap = styled.div`
+const CommentWrap = styled.form`
   padding: 30px 0 30px 40px;
   background-color: aliceblue;
 `;
 
-const CommentUserTitle = styled.p`
-  width: 80px;
+const CommentUserName = styled.p`
+  width: 100px;
   height: 20px;
   background-color: #d9d9d9;
 `;
 
-const CommentUserWriting = styled.p`
+const CommentUserWriting = styled.input`
   margin: 8px 0 8px 0;
-`;
-
-const CommentDate = styled.p`
-  font-weight: 400;
 `;
 
 const CommentButton = styled.button``;
