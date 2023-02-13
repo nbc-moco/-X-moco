e377ccf0d5cfc7ea0a52bc7c5605b46f5d0a7073
coachkim <kimsw0825@gmail.com>
2023-02-13 09:15:56 +0900

    design: 슬라이드배너, 헤더 UI수정

--- src/components/homestyle/homebanner.js
+++ src/components/homestyle/homebanner.js
@@ -1,12 +1,52 @@
 import styled from "@emotion/styled";
+import Slider from "react-slick";
 
 /* HomeBanner */
 const BannerContainer = styled.div`
     background-color: #D9D9D9;
-    width: 1920px;
     height: 300px;
 `
-/* HomeBanner */
+const SliderArea = styled.div`
+    width: 1180px;
+    height: 300px;
+    margin: 0 auto;
+`
+const StyledSlider = styled(Slider)`
+    height: 100%;
+    width: 100%;
+    position: relative;
+    .slick-prev::before,
+    .slick-next::before {
+        opacity: 0;
+        display: none;
+    }
+    /* display: flex;
+    justify-content: center;
+    align-items: center; */
+`
+const Pre = styled.div`
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  left: 3%;
+  z-index: 3;
+`;
+const NextTo = styled.div`
+  width: 30px;
+  height: 30px;
+  position: absolute;
+  right: 3%;
+  z-index: 3;
+`;
+const Banner = styled.div`
+    width: 100%;
+    height: 300px;
+    background-color: gray;
+    text-align: center;
+    font-size: 50px;
+    flex-wrap: wrap;
+`
+/* HomeGuideText */
 const GuideContainer = styled.div`
     height: 266px;
     width: 1920px;
@@ -22,4 +62,4 @@ const GuideText = styled.div`
     text-align: center;
 `
 
-export { BannerContainer, GuideContainer, GuideText };
+export { BannerContainer, SliderArea, StyledSlider, Pre, NextTo, Banner, GuideContainer, GuideText };
