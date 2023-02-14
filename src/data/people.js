d1f05b9688de7f882df9b11a95f1f9f3c085fde2
wallofwolf <77008335+wallofwolf@users.noreply.github.com>
2023-02-14 16:51:58 +0900

    feat : 모집 글 작성 기능 추가

---
+++ src/data/people.js
@@ -0,0 +1,9 @@
+export const people = [
+  { value: '1명', label: '1명' },
+  { value: '2명', label: '2명' },
+  { value: '3명', label: '3명' },
+  { value: '4명', label: '4명' },
+  { value: '5명', label: '5명' },
+  { value: '6명', label: '6명' },
+  { value: '7명 이상', label: '7명 이상' },
+];
