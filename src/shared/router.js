d3dca594b6324ac119f55dca2f83f4a62af4518b
dada992 <olive_js_933@naver.com>
2023-02-15 14:26:31 +0900

    [design] 팀 페이지 ui 중간

--- src/shared/router.js
+++ src/shared/router.js
@@ -9,6 +9,7 @@ import Home from './../pages/home/Home';
 import MateDetail from './../pages/mate/MateDetail';
 import MateWrite from '../pages/mate/MateWrite';
 import OnboardingPage from '../pages/onboarding/OnboardingPage';
+import TeamPage from '../pages/teampage/TeamPage';
 
 const Router = () => {
   // path 이름은 보통 소문자로 하니, 저희도 소문자로 통일하겠습니다
@@ -22,6 +23,8 @@ const Router = () => {
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/matedetail" element={<MateDetail />} />
         <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route path="/teampage" element={<TeamPage />} />
+        <Route path="/teampage/:uid" element={<TeamPage />} />
         <Route path="/mate" element={<MateList />} />
         <Route path="/write" element={<MateWrite />} />
       </Routes>
